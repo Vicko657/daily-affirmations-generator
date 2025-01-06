62b82b36804ca37f02517f253381a8323e647cf8
Vicko657 <victoriaolusegun@gmail.com>
2025-01-06 01:28:35 +0000

    added AI Api Integration

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-function generateAffirmations(event) {
-  event.preventDefault();
-  alert("Generating affirmations");
-
+function displayAffirmations(response) {
+  console.log("Affirmations generated");
   new Typewriter("#affirmation", {
-    strings: "I am good enough",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 30,
   });
+}
+
+function generateAffirmations(event) {
+  event.preventDefault();
+  alert("Generating affirmations");
+
+  let wordInput = document.querySelector("#users-word");
+
+  let apiKey = "t6a8db7630cb9o483fccd0984e54c304";
+  let prompt = `User Instructions: Generate affirmations based on ${wordInput.value}`;
+  let context =
+    "You are a mental health expert in affirmations. Create a list of 8 affirmations in basic HTML. No bullets, links, titles and code should be seen. Please keep them short, appropriate and follow the user instructions. Sign the affirmations with 'SheCodes AI inside a <strong> element";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayAffirmations);
+  console.log("Generating affirmations");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
 }
 
 let affirmationFormElement = document.querySelector(
