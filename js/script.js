0b208da8bd250c3d9c27e1c5bfaf37e437e0a7d8
Vicko657 <victoriaolusegun@gmail.com>
2025-02-18 01:27:31 +0000

    Updated styling

--- js/script.js
+++ js/script.js
@@ -10,22 +10,22 @@ function displayAffirmations(response) {
 function generateAffirmations(event) {
   event.preventDefault();
 
-  let wordInput = document.querySelector("#users-word");
+  const wordInput = document.querySelector("#users-word");
 
-  let apiKey = "t6a8db7630cb9o483fccd0984e54c304";
-  let prompt = `User Instructions: Generate affirmations based on ${wordInput.value}`;
-  let context =
+  const apiKey = "t6a8db7630cb9o483fccd0984e54c304";
+  const prompt = `User Instructions: Generate affirmations based on ${wordInput.value}`;
+  const context =
     "You are a mental health expert in affirmations. Create a list of 8 affirmations in basic HTML. No bullets, links, titles and code should be seen. Please keep them short, appropriate and follow the user instructions. Sign the affirmations with 'SheCodes AI inside a <strong> element";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayAffirmations);
 
-  let affirmationElement = document.querySelector("#affirmation");
+  const affirmationElement = document.querySelector("#affirmation");
   affirmationElement.classList.remove("hidden");
   affirmationElement.innerHTML = `⏳ Generating a list of affirmations on your ${wordInput.value} emotion`;
 }
 
-let affirmationFormElement = document.querySelector(
+const affirmationFormElement = document.querySelector(
   "#affirmation-generator-form"
 );
 affirmationFormElement.addEventListener("submit", generateAffirmations);
